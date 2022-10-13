0d551918152968018a228b321f8edad149464afa
Ollie <code@ollie.codes>
2022-10-13 13:18:06 +0100

    chore: Added an early false return for ComparisonHelper::contains()

--- src/Helpers/IterableHelper.php
+++ src/Helpers/IterableHelper.php
@@ -49,6 +49,8 @@ final class IterableHelper
                     return true;
                 }
             }
+
+            return false;
         }
 
         return in_array($element, $iterable, true);
