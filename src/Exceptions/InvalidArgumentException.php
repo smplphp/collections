5cbb13b9bde5512d22419091ce835e9c5d99bc82
Ollie <code@ollie.codes>
2022-10-15 14:44:28 +0100

    chore: Added missing base invalid argument exception

---
+++ src/Exceptions/InvalidArgumentException.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace Smpl\Collections\Exceptions;
+
+use InvalidArgumentException as BaseInvalidArgumentException;
+
+final class InvalidArgumentException extends BaseInvalidArgumentException
+{
+
+}
