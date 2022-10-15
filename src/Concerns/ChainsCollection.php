a07751d13bc6f349c7b4baf6abdacdc78247fe12
Ollie <code@ollie.codes>
2022-10-15 14:43:46 +0100

    feat: Added chainable collection contract and concern for simple implementation
    Chainable collections extend mutable collections by adding wrappers for add, remove, removeIf, removeAll and retainAll, that always return the current collection

---
+++ src/Concerns/ChainsCollection.php
@@ -0,0 +1,103 @@
+<?php
+declare(strict_types=1);
+
+namespace Smpl\Collections\Concerns;
+
+use Smpl\Collections\Contracts\Predicate;
+
+/**
+ * Chains Collection Concerns
+ *
+ * This concern exists as an implementation of {@see \Smpl\Collections\Contracts\ChainableCollection}
+ * to simplify its usage, as most of its methods are designed to proxy other
+ * methods founds in {@see \Smpl\Collections\Contracts\MutableCollection}.
+ *
+ * @template I of mixed
+ * @template E of mixed
+ * @requires \Smpl\Collections\Contracts\ChainableCollection<I, E>
+ * @mixin \Smpl\Collections\Contracts\MutableCollection<I, E>
+ */
+trait ChainsCollection
+{
+    /**
+     * @param E $element
+     *
+     * @return static
+     *
+     * @throws \Smpl\Collections\Exceptions\InvalidArgumentException
+     *
+     * @see \Smpl\Collections\Contracts\MutableCollection::add()
+     */
+    public function push(mixed $element): static
+    {
+        $this->add($element);
+        return $this;
+    }
+
+    /**
+     * @param iterable<E> $elements
+     *
+     * @return static
+     *
+     * @throws \Smpl\Collections\Exceptions\InvalidArgumentException
+     *
+     * @see \Smpl\Collections\Contracts\MutableCollection::addAll()
+     */
+    public function pushAll(iterable $elements): static
+    {
+        $this->addAll($elements);
+        return $this;
+    }
+
+    /**
+     * @param E $element
+     *
+     * @return static
+     *
+     * @see \Smpl\Collections\Contracts\MutableCollection::remove()
+     */
+    public function forget(mixed $element): static
+    {
+        $this->remove($element);
+        return $this;
+    }
+
+    /**
+     * @param iterable<E> $elements
+     *
+     * @return static
+     *
+     * @see \Smpl\Collections\Contracts\MutableCollection::removeAll()
+     */
+    public function forgetAll(iterable $elements): static
+    {
+        $this->removeAll($elements);
+        return $this;
+    }
+
+    /**
+     * @param \Smpl\Collections\Contracts\Predicate<E> $filter
+     *
+     * @return static
+     *
+     * @see \Smpl\Collections\Contracts\MutableCollection::removeIf()
+     */
+    public function forgetIf(Predicate $filter): static
+    {
+        $this->removeIf($filter);
+        return $this;
+    }
+
+    /**
+     * @param iterable<E> $elements
+     *
+     * @return static
+     *
+     * @see \Smpl\Collections\Contracts\MutableCollection::retainAll()
+     */
+    public function keepAll(iterable $elements): static
+    {
+        $this->retainAll($elements);
+        return $this;
+    }
+}
---
+++ src/Contracts/ChainableCollection.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace Smpl\Collections\Contracts;
+
+/**
+ * Chainable Collection Contract
+ *
+ * This contract is an extension of {@see \Smpl\Collections\Contracts\MutableCollection},
+ * providing chainable alternatives to its methods that modify the collection,
+ * returning a boolean.
+ *
+ * The idea behind the methods contained within this contract are purely for the
+ * purpose of convenience, as sometimes you need to worry about the result of an
+ * operation, just that it happened.
+ *
+ * @template I of mixed
+ * @template E of mixed
+ * @extends \Smpl\Collections\Contracts\MutableCollection<I, E>
+ */
+interface ChainableCollection extends MutableCollection
+{
+    /**
+     * Push an element onto this collection.
+     *
+     * This method will add the provided element to this collection, if it can be.
+     *
+     * This method must function identically to
+     * {@see \Smpl\Collections\Contracts\MutableCollection::add()}, except that
+     * the collection instance is returned regardless of whether the collection
+     * was modified.
+     *
+     * @param E $element
+     *
+     * @return static
+     *
+     * @throws \Smpl\Collections\Exceptions\InvalidArgumentException
+     *
+     * @see \Smpl\Collections\Contracts\MutableCollection::add()
+     */
+    public function push(mixed $element): static;
+
+    /**
+     * Push all the provided elements onto this collection.
+     *
+     * This method will add all the provided elements to this collection, if they
+     * can be.
+     *
+     * This method must function identically to
+     * {@see \Smpl\Collections\Contracts\MutableCollection::addAll()}, except that
+     * the collection instance is returned regardless of whether the collection
+     * was modified.
+     *
+     * @param iterable<E> $elements
+     *
+     * @return static
+     *
+     * @throws \Smpl\Collections\Exceptions\InvalidArgumentException
+     *
+     * @see \Smpl\Collections\Contracts\MutableCollection::addAll()
+     */
+    public function pushAll(iterable $elements): static;
+
+    /**
+     * Forget the provided element.
+     *
+     * This method will remove the provided element from this collection, if it
+     * can be.
+     *
+     * This method must function identically to
+     * {@see \Smpl\Collections\Contracts\MutableCollection::remove()}, except that
+     * the collection instance is returned regardless of whether the collection
+     * was modified.
+     *
+     * @param E $element
+     *
+     * @return static
+     *
+     * @see \Smpl\Collections\Contracts\MutableCollection::remove()
+     */
+    public function forget(mixed $element): static;
+
+    /**
+     * Forget all the provided elements.
+     *
+     * This method will remove all the provided elements from this collection, if
+     * they can be.
+     *
+     * This method must function identically to
+     * {@see \Smpl\Collections\Contracts\MutableCollection::removeAll()}, except that
+     * the collection instance is returned regardless of whether the collection
+     * was modified.
+     *
+     * @param iterable<E> $elements
+     *
+     * @return static
+     *
+     * @see \Smpl\Collections\Contracts\MutableCollection::removeAll()
+     */
+    public function forgetAll(iterable $elements): static;
+
+    /**
+     * Forget all elements that pass the provided filter.
+     *
+     * This method will remove all elements from this collection that pass
+     * the provided filter.
+     *
+     * This method must function identically to
+     * {@see \Smpl\Collections\Contracts\MutableCollection::removeIf()}, except that
+     * the collection instance is returned regardless of whether the collection
+     * was modified.
+     *
+     * @param \Smpl\Collections\Contracts\Predicate<E> $filter
+     *
+     * @return static
+     *
+     * @see \Smpl\Collections\Contracts\MutableCollection::removeIf()
+     */
+    public function forgetIf(Predicate $filter): static;
+
+    /**
+     * Forget all elements not in the provided elements.
+     *
+     * This method will remove all elements from this collection that are not
+     * contained in the provided elements.
+     *
+     * This method must function identically to
+     * {@see \Smpl\Collections\Contracts\MutableCollection::retainAll()}, except that
+     * the collection instance is returned regardless of whether the collection
+     * was modified.
+     *
+     * @param iterable<E> $elements
+     *
+     * @return static
+     *
+     * @see \Smpl\Collections\Contracts\MutableCollection::retainAll()
+     */
+    public function keepAll(iterable $elements): static;
+}
