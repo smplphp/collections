e7a05e2e1020d3359e319676e67b09aa79d5b408
Ollie <code@ollie.codes>
2022-10-25 14:50:54 +0100

    test(iterators): Tidied up and simplified the simple iterator

--- tests/Iterators/SimpleIteratorTest.php
+++ tests/Iterators/SimpleIteratorTest.php
@@ -4,51 +4,48 @@ declare(strict_types=1);
 namespace Smpl\Collections\Tests\Iterators;
 
 use PHPUnit\Framework\TestCase;
+use Smpl\Collections\Exceptions\OutOfRangeException;
+use Smpl\Collections\Iterators\SequenceIterator;
 use Smpl\Collections\Iterators\SimpleIterator;
+use Smpl\Collections\Sequence;
 
 /**
- * @group simple-iterator
+ * @group iterator
+ * @group simple
  */
 class SimpleIteratorTest extends TestCase
 {
-    /**
-     * @var int[]
-     */
-    private array $elements;
-
-    protected function setUp(): void
+    public function elementIteratorProvider(): array
     {
-        $this->elements = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        $iterator = new SimpleIterator([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+        return [
+            'Index 0'  => [$iterator, 0, 0, true],
+            'Index 1'  => [$iterator, 1, 1, true],
+            'Index 2'  => [$iterator, 2, 2, true],
+            'Index 3'  => [$iterator, 3, 3, true],
+            'Index 4'  => [$iterator, 4, 4, true],
+            'Index 5'  => [$iterator, 5, 5, true],
+            'Index 6'  => [$iterator, 6, 6, true],
+            'Index 7'  => [$iterator, 7, 7, true],
+            'Index 8'  => [$iterator, 8, 8, true],
+            'Index 9'  => [$iterator, 9, 9, true],
+            'Index 10' => [$iterator, null, false, false],
+            'Index 11' => [$iterator, null, false, false],
+        ];
     }
 
     /**
      * @test
+     * @dataProvider elementIteratorProvider
      */
-    public function iteratesForAllElements(): void
+    public function iteratesForAllElements(SimpleIterator $iterator, ?int $key, int|bool $current, bool $valid): void
     {
-        $iterator = new SimpleIterator($this->elements);
-        $data = [
-            0  => [0, 0, true],
-            1  => [1, 1, true],
-            2  => [2, 2, true],
-            3  => [3, 3, true],
-            4  => [4, 4, true],
-            5  => [5, 5, true],
-            6  => [6, 6, true],
-            7  => [7, 7, true],
-            8  => [8, 8, true],
-            9  => [9, 9, true],
-            10 => [null, false, false],
-            11 => [null, false, false],
-        ];
+        self::assertSame($key, $iterator->key());
+        self::assertSame($valid, $iterator->valid());
+        self::assertSame($current, $iterator->current());
 
-        foreach ($data as [$key, $current, $valid]) {
-            self::assertSame($key, $iterator->key());
-            self::assertSame($current, $iterator->current());
-            self::assertSame($valid, $iterator->valid());
-
-            $iterator->next();
-        }
+        $iterator->next();
     }
 
     /**
@@ -56,7 +53,7 @@ class SimpleIteratorTest extends TestCase
      */
     public function isRewindable(): void
     {
-        $iterator = new SimpleIterator($this->elements);
+        $iterator = new SimpleIterator([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
         $data = [
             0  => [0, 0, true],
             1  => [1, 1, true],
