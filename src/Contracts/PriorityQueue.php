174eb70d7b291c036ff2072f05c48766d296356f
Ollie <code@ollie.codes>
2022-10-30 14:12:40 +0000

    chore: Added missing docblock for PriorityQueue

--- src/Contracts/PriorityQueue.php
+++ src/Contracts/PriorityQueue.php
@@ -3,6 +3,11 @@
 namespace Smpl\Collections\Contracts;
 
 /**
+ * Priority Queue Contract
+ *
+ * This contract represents a specific variant of {@see \Smpl\Collections\Contracts\Queue},
+ * that allows you to add elements with a numeric priority.
+ *
  * @template E of mixed
  * @extends \Smpl\Collections\Contracts\Queue<E>
  */
